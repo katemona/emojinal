fff45ecf6e487b987aea4f622a7d45bfb16525be
Kate Monahan <katemona514@gmail.com>
2019-07-12 11:51:37 -0500

    Final

--- ViewController.swift
+++ ViewController.swift
@@ -12,11 +12,14 @@ class ViewController: UIViewController
 {
 
     var customMessages = [
-        "You Should Get..." : ["McDonald's", "Culver's", "Taco Bell"],
-        "You Should Try..." : ["Cheesecake Factory", "River Roast", "Greek Islands"]
+        "You Should Get..." : ["McDonald's", "Culver's", "Taco Bell", "Wendy's"],
+        "You Should Try..." : ["Cheesecake Factory", "River Roast", "Greek Islands", "Maggiano's"],
+        "You Should Eat..." : ["Insomnia Cookies", "Stan's Donuts", "Sprinkles Cupcakes", "Weber's Bakery"]
+        
 ]
 
-    let emojis = ["🥡" : "You Should Get...", "🍽" : "You Should Try..."]
+    let emojis = ["🥡" : "You Should Get...", "🍽" : "You Should Try...", "🍪" : "You Should Eat..."
+        ]
     
     override func viewDidLoad()
 {
@@ -54,6 +57,20 @@ class ViewController: UIViewController
         
         present(alertController, animated: true, completion: nil)
 }
+
+    @IBAction func showThird(sender: UIButton)
+    {
+        let selectedEmotion = sender.titleLabel?.text
+        
+        let random: UInt32 = arc4random_uniform(3)
+        
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int(random)]
+        
+        let alertController = UIAlertController(title: emojis[selectedEmotion!], message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    }
 
     
 }
