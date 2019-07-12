679e44cf8bb2c96c8e38e7f9e7b8eb911c98188e
Kate Monahan <katemona514@gmail.com>
2019-07-12 11:38:31 -0500

    I3

--- ViewController.swift
+++ ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    var customMessages = [
+        "You Should Get..." : ["McDonald's", "Culver's", "Taco Bell"],
+        "You Should Try..." : ["Cheesecake Factory", "River Roast", "Greek Islands"]
+]
+
+    let emojis = ["🥡" : "You Should Get...", "🍽" : "You Should Try..."]
+    
+    override func viewDidLoad()
+{
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+}
 
+    @IBAction func showMessage(sender: UIButton)
+{
 
+        let selectedEmotion = sender.titleLabel?.text
+    
+    let random: UInt32 = arc4random_uniform(3)
+    
+    let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int(random)]
+
+        
+    let alertController = UIAlertController(title: emojis[selectedEmotion!], message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+            present(alertController, animated: true, completion: nil)
+    
+}
+
+    @IBAction func showSecond(sender: UIButton)
+{
+    let selectedEmotion = sender.titleLabel?.text
+
+    let random: UInt32 = arc4random_uniform(3)
+    
+    let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[Int(random)]
+        
+    let alertController = UIAlertController(title: emojis[selectedEmotion!], message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+}
+
+    
 }
 
